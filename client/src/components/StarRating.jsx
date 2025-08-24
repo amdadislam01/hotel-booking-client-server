f843b7773fe08c773b972a6ce3124626ee4b1b5d
amdadislam01 <amdadislam733@gmail.com>
2025-08-24 18:21:41 +0600

    Footer Complate

---
+++ client/src/components/StarRating.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const StarRating = ({ rating = 4 }) => {
+  return (
+    <>
+      {Array(5)
+        .fill(0)
+        .map((_, index) => (
+          <img
+            className="w-4.5 h-4.5"
+            src={
+              rating > index ? assets.starIconFilled : assets.starIconOutlined
+            }
+            alt=""
+          />
+        ))}
+    </>
+  );
+};
+
+export default StarRating;
